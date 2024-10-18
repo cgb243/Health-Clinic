ddb2e379cf41d2a5c4f5518ff157153a1a26bcb7
Chase Blancher <chasegb1215@gmail.com>
2024-10-18 11:32:45 -0400

    Continued Adding the API service for Patients

--- COP4870-2/API.Health.Clinic/API.Health.Clinic/Enterprise/PatientEC.cs
+++ COP4870-2/API.Health.Clinic/API.Health.Clinic/Enterprise/PatientEC.cs
@@ -16,4 +16,24 @@ public class PatientEC
             return FakeDatabase.Patients;
         }
     }
+
+    public Patient? GetById(int id)
+    {
+        return FakeDatabase.Patients.FirstOrDefault(p => p.Id == id);
+    }
+
+    public Patient? Delete(int id)
+    {
+        var patientToDelete = FakeDatabase.Patients.FirstOrDefault(p => p.Id == id);
+        if (patientToDelete != null)
+        {
+            FakeDatabase.Patients.Remove(patientToDelete);
+        }
+        return patientToDelete;
+    }
+
+    public Patient? AddOrUpdate(Patient? patient)
+    {
+        return FakeDatabase.AddOrUpdatePatient(patient);
+    } 
 }
